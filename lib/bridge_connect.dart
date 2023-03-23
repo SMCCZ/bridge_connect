ff992393e73c28b43321853eedb1cb8039c8923b
SMCCZ <sabir.mondal@cloudcraftz.io>
2023-03-23 16:23:25 +0530

    POST API call initliated

--- lib/bridge_connect.dart
+++ lib/bridge_connect.dart
@@ -1,7 +1,25 @@
 library bridge_connect;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class BridgeConnect {
+  static Future<Map<String, dynamic>> postAPICall(
+      {required String api,
+      required Map<String, String> body,
+      required Map<String, String> headersList}) async {
+    var url = Uri.parse(api);
+    var req = Request('POST', url);
+    req.headers.addAll(headersList);
+    req.body = jsonEncode(body);
+    var res = await req.send();
+    final resBody = await res.stream.bytesToString();
+    var json = jsonDecode(resBody);
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      return json;
+    } else {
+      return json;
+    }
+  }
 }
