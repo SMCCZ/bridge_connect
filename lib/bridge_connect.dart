9ff5b4a9c09a4c8984d2d9a9764a2138e071d615
SMCCZ <sabir.mondal@cloudcraftz.io>
2023-03-28 15:26:31 +0530

    GET API Completed

--- lib/bridge_connect.dart
+++ lib/bridge_connect.dart
@@ -8,11 +8,26 @@ class BridgeConnect {
   static Future<Map<String, dynamic>> postAPICall(
       {required String api,
       required Map<String, String> body,
-      required Map<String, String> headersList}) async {
+      Map<String, String>? headersList}) async {
     var url = Uri.parse(api);
     var req = Request('POST', url);
-    req.headers.addAll(headersList);
+    req.headers.addAll(headersList ?? {'Accept': '*/*'});
     req.body = jsonEncode(body);
+    var res = await req.send();
+    final resBody = await res.stream.bytesToString();
+    var json = jsonDecode(resBody);
+    if (res.statusCode >= 200 && res.statusCode < 300) {
+      return json;
+    } else {
+      return json;
+    }
+  }
+
+  static Future<Map<String, dynamic>> getAPICall(
+      {required String api, Map<String, String>? headersList}) async {
+    var url = Uri.parse(api);
+    var req = Request('GET', url);
+    req.headers.addAll(headersList ?? {'Accept': '*/*'});
     var res = await req.send();
     final resBody = await res.stream.bytesToString();
     var json = jsonDecode(resBody);
--- test/bridge_connect_test.dart
+++ test/bridge_connect_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bridge_connect/bridge_connect.dart';
-
 void main() {
   test('adds one to input values', () {
     // final calculator = Calculator();
